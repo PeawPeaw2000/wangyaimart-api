f396a951647fb03ca4c4c531d8cde2fef6a15f42
nonthakan2000 <93617352+nonthakan2000@users.noreply.github.com>
2022-07-12 01:56:00 +0700

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'ormconfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(), , AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
