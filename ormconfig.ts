aa5f5f5ff133b60bdd63e718f86f675841bf67f6
nonthakan2000 <93617352+nonthakan2000@users.noreply.github.com>
2022-07-12 01:51:35 +0700

    Update ormconfig.ts

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+require('dotenv').config();
+
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: +process.env.MYSQL_WRITE_PORT,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+};
+
+export default config;
