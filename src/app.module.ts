ef807493317a8037de2c5171e0e588c7af7ce650
nonthakan2000 <93617352+nonthakan2000@users.noreply.github.com>
2022-07-12 00:44:02 +0700

    Initial database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,8 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import config from 'ormconfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(config), AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(), , AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
