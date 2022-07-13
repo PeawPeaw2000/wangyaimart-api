2fc4dbdcfa5c311f58cc60dd3a9473f05a9fbbd6
nonthakan2000 <93617352+nonthakan2000@users.noreply.github.com>
2022-07-14 00:35:49 +0700

    [update] payload Jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.email, sub: user.id };
+    const payload = { id: user.id, email: user.email, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
     };
